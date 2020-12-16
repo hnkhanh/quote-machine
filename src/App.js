fb6a6d4c70c1d7bce1c1c44a155937561e82c955
hnkhanh <70878801+hnkhanh@users.noreply.github.com>
2020-12-16 22:19:52 +0700

    prop

--- src/App.js
+++ src/App.js
@@ -6,15 +6,12 @@ import {random} from 'lodash'
 const API = 'https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
+  state = {
       quotes: [],
       index: 0,
     }
-  }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch(API)
       .then(data => data.json())
       .then(quotes => this.setState({ quotes }, this.generateIndex))
